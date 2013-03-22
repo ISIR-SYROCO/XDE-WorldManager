150a75d353db6a504956b72e7a903e8d2182fa31
Joseph Salini <salini@isir.upmc.fr>
2013-03-22 01:26:27 +0100

    add documentation into the core module (not finished yet)

--- src/core.py
+++ src/core.py
@@ -21,20 +21,44 @@ import agents.physic.builder
 verbose = False
 
 def verbose_print(msg):
-    if verbose is True:
-        print(msg)
+	if verbose is True:
+		print(msg)
 
 class WorldManager():
+	""" Middle layer to manage the different XDE agents.
+	
+	It always creates a clock agent (clock) and a physic agent (phy).
+	This give access to the main physical scene (ms), the geometrical scene (xcd)
+	and a sync connector (icsync) to synchronize physic with asynchronous events.
+	
+	Besides, one can create a graphic agent (graph) which gives access to the
+	graphical scene (graph_scn).
 	"""
-	"""
+	
 	def __init__(self, corba=False):
-		self.phy = None
-		self.graph = None
-		self.clock = None
-		self.ms = None
-		self.xcd = None
+		""" Initialize WorldManager instance.
+		
+		:param corba: If corba is set to True, it allows to make a proxy of a
+					distant physic agent. See methods:
+					* getPhysicAgentFromCorba
+					* getGraphicAgentFromCorba
+		
+		This world manager give access to the following agents/scene (when created, None by default):
+		* phy: the physical agent
+		* graph: the graphical agent
+		* clock: the clock agent, to get current time & periodically call physic
+		* ms: the main physical scene, to get segment, joint, etc...
+		* xcd: the geometrical scene, to get collision information, composite, etc...
+		* graph_scn: the main graphical scene, to interacte with the displayed elements
+		* icsync: the physic IConnectorSynchro, to make synchronization with the 'addEvent' method
+		"""
+		self.phy       = None
+		self.graph     = None
+		self.clock     = None
+		self.ms        = None
+		self.xcd       = None
 		self.graph_scn = None
-		self.icsync = None
+		self.icsync    = None
 		
 		self._internal_z = 1
 		
@@ -43,12 +67,34 @@ class WorldManager():
 			rtt_interface_corba.Init(sys.argv)
 
 
-	def createClockAgent(self, time_step):
+	def createClockAgent(self, time_step, clock_name="clock"):
+		""" Create clock agent.
+		
+		:param time_step: the tick period (in s)
+		:param clock_name: the name of the clock agent
+		
+		It initialize the following property in this world manager:
+		* clock
+		"""
 		verbose_print("CREATE CLOCK...")
-		self.clock = dsimi.rtt.Task(ddeployer.load("clock", "dio::Clock", "dio-cpn-clock", "dio/component/"))
+		self.clock = dsimi.rtt.Task(ddeployer.load(clock_name, "dio::Clock", "dio-cpn-clock", "dio/component/"))
 		self.clock.s.setPeriod(time_step)
 
 	def createPhysicAgent(self, time_step, dt, phy_name, lmd_max=0.01, uc_relaxation_factor=0.1):
+		""" Create physical agent.
+		
+		:param time_step: the period which call the physic update
+		:param dt: the physic integration time, can be different of the update call period
+		:param phy_name: the name of the physical agent
+		:param lmd_max: (local minimal distance) the threshold when the lmd is considered (beyond, global distance are considered)
+		:param uc_relaxation_factor: TODO!! if I remember well, it is related to the force distribution when two planes are in contact
+		
+		It initialize the following properties in this world manager:
+		* phy
+		* ms
+		* xcd
+		* icsync
+		"""
 		verbose_print("CREATE PHYSIC...")
 		self.phy = agents.physic.core.createAgent(phy_name, 0)
 		self.phy.s.setPeriod(time_step)
@@ -69,6 +115,12 @@ class WorldManager():
 		self.icsync.addEvent("clock_trigger")
 
 	def createGraphicAgent(self, graph_name):
+		""" Create graphical agent.
+		
+		:param graph_name: the name of the graphical agent
+		
+		By default, the transparent ground is not shown. See 'graph_scn.SceneryInterface.showGround' to show it.
+		"""
 		verbose_print("CREATE GRAPHIC...")
 		self.graph = agents.graphic.simple.createAgent(graph_name, 0)
 
@@ -78,7 +130,7 @@ class WorldManager():
 		agents.graphic.proto.configureBasicLights(self.graph_scn)
 		agents.graphic.proto.configureBasicCamera(self.graph_scn)
 		self.graph.s.Viewer.enableNavigation(True)
-		self.graph_scn.SceneryInterface.showGround(True)
+		self.graph_scn.SceneryInterface.showGround(False)
 
 		self.graph.s.Connectors.IConnectorBody.new("icb", "body_state_H", scene_name)    #to show bodies
 		self.graph.s.Connectors.IConnectorFrame.new("icf", "framePosition", scene_name)  #to link with frames/markers
@@ -87,6 +139,7 @@ class WorldManager():
 		self.graph.s.start()
 
 	def connectGraphToPhysic(self):
+		""" Connect graphical & physical agents, to show/update bodies/markers/contacts. """
 		assert(self.graph is not None)
 		assert(self.phy is not None)
 		self.graph.getPort("body_state_H").connectTo(self.phy.getPort("body_state_H"))
@@ -94,6 +147,7 @@ class WorldManager():
 		self.graph.getPort("contacts").connectTo(self.phy.getPort("contacts"))
 
 	def disconnectGraphFromPhysic(self):
+		""" Disconnect graphical & physical agents. """
 		assert(self.graph is not None)
 		assert(self.phy is not None)
 
@@ -101,8 +155,10 @@ class WorldManager():
 		self.graph.getPort("framePosition").disconnect()
 		self.graph.getPort("contacts").disconnect()
 
-
+	# TODO: this method is deprecated, problem to connect physic & graphic agent
+	#       we don't know how to load graph data with corba, if world has been created elsewhere
 	def changePhy(self, phy_name):
+		""" Problem or depracated, no doc done. """
 		self.disconnectGraphFromPhysic()
 		self.cleanGraph()
 		self.getPhysicAgentFromCorba(phy_name)
@@ -112,11 +168,16 @@ class WorldManager():
 		self.connectGraphToPhysic()
 
 	def connectClockToPhysic(self):
+		""" Connect clock to physic, to trigger phy update with the clock tick """
 		assert(self.clock is not None)
 		assert(self.phy is not None)
 		self.clock.getPort("ticks").connectTo(self.phy.getPort("clock_trigger"))
 
 	def startAgents(self):
+		""" Start clock & physical agents.
+		
+		Note: the graphical agent, if created, is started when created.
+		"""
 		if (self.clock is not None):
 			self.clock.s.start()
 		if (self.phy is not None):
@@ -124,6 +185,8 @@ class WorldManager():
 
 
 	def stopAgents(self):
+		""" Stop clock & physical agents.
+		"""
 		if (self.clock is not None):
 			self.clock.s.stop()
 		if (self.phy is not None):
@@ -131,13 +194,15 @@ class WorldManager():
 
 
 	def createAllAgents(self, time_step, dt=None, phy_name="physic", lmd_max=0.01, uc_relaxation_factor=0.1, create_graphic=True, graph_name = "graphic"):
-		"""
-		Create and configure graphic, physic agent and a clock task.
-		Basic connectors are created in the graph agent:
-		icb     Body state
-		icf     Frame position
-		icc     Contact information
-		Physic is sync-ed with the clock
+		""" Create all agents: clock, physic & graphic (if required).
+		
+		:param time_step: the period of time to call update for clock (Warning: phy is synchronized with clock, the update is called by clock tick, not period)
+		:param dt: the integration time for physic at each update call
+		:param phy_name: the name of the physical agent
+		:param lmd_max: (local minimal distance) the threshold when the lmd is considered (beyond, global distance are considered)
+		:param uc_relaxation_factor: TODO!! if I remember well, it is related to the force distribution when two planes are in contact
+		:param create_graphic: if False, no physical agent is created; then, 'graph' & 'graph_scn' remain None
+		:param graph_name: the name of the graphical agent
 		"""
 		self.createClockAgent(time_step)
 		self.createPhysicAgent(time_step, dt, phy_name, lmd_max, uc_relaxation_factor)
@@ -149,11 +214,18 @@ class WorldManager():
 
 
 	def createAndConnectGraphicAgent(self, graph_name):
+		""" Create graphical agent & connect it with the physical agent. """
 		self.createGraphicAgent(graph_name)
 		self.connectGraphToPhysic()
 
 
 	def getPhysicAgentFromCorba(self, phy_name):
+		""" If corba has been set to True, proxy a remote physical agent.
+		
+		It also recovers ms (main scene) and xcd (geometrical scene).
+		
+		:param phy_name: the remote physical agent name
+		"""
 		if self.corba:
 			phy_p = rtt_interface_corba.GetProxy(phy_name, False)
 			self.phy = dsimi.rtt.Task(phy_p, binding_class = dsimi.rtt.ObjectStringBinding, static_classes=['agent'])
@@ -165,6 +237,12 @@ class WorldManager():
 
 
 	def getGraphicAgentFromCorba(self, graph_name):
+		""" If corba has been set to True, proxy a remote graphical agent.
+		
+		TODO: check if this function work properly, does not know how the data are transfered.
+		
+		:param graph_name: the remote graphical agent name
+		"""
 		if self.corba:
 			graph_p = rtt_interface_corba.GetProxy(graph_name, False)
 			self.graph = dsimi.rtt.Task(graph_p, binding_class = dsimi.rtt.ObjectStringBinding, static_classes=['agent'])
@@ -179,6 +257,12 @@ class WorldManager():
 
 
 	def addWorldToPhysic(self, new_world):
+		""" Deserialize a new World into the physical components.
+		
+		It fills the phy, ms and xcd elements of the WorldManager instance.
+		
+		:param new_world: a scene_pb2.World which will be deserialized
+		"""
 		phy = self.phy
 		scene = self.ms
 		Lmat = new_world.scene.physical_scene.contact_materials
@@ -194,6 +278,13 @@ class WorldManager():
 
 
 	def addWorldToGraphic(self, new_world):
+		""" Deserialize a new World into the graphical components.
+		
+		It fills the graph and graph_scn elements of the WorldManager instance.
+		Note that if create_graphic has been set to False, nothing will be done.
+		
+		:param new_world: a scene_pb2.World which will be deserialized
+		"""
 		if (self.graph is not None):
 			agents.graphic.builder.deserializeWorld(self.graph, self.graph_scn, new_world)
 
@@ -206,13 +297,13 @@ class WorldManager():
 
 
 	def addWorld(self, new_world):
-		"""
-		Add the world description into the simulation:
-		Deserialize physic, graphic removing redundant material description
-		and adding the body state to the OConnectorBodyStateList.
-
-		stop_simulation     if True, the simulation is paused after the deserialization
-							it can be useful to initialize other things.
+		""" Deserialize a new World into the graphical & physical components.
+		
+		:param new_world: a scene_pb2.World which will be deserialized
+		
+		For more information, see methods:
+		* addWorldToPhysic
+		* addWorldToGraphic
 		"""
 		phy_name = self.phy.getName()
 
@@ -222,12 +313,16 @@ class WorldManager():
 		self.addWorldToGraphic(new_world)
 
 
-	def removeWorldFromGraphicAgent(self, world):
+	def removeWorldFromGraphic(self, old_world):
+		""" Remove a World from all the graphical components.
+		
+		:param old_world: a scene_pb2.World which will be deleted
+		"""
 		if (self.graph is not None):
 			verbose_print("REMOVE CONNECTION PHY/GRAPH...")
 			ocb = self.phy.s.Connectors.OConnectorBodyStateList("ocb")
 
-			for b in world.scene.rigid_body_bindings:
+			for b in old_world.scene.rigid_body_bindings:
 				if len(b.graph_node) and len(b.rigid_body):
 					ocb.removeBody(str(b.rigid_body))
 
@@ -240,10 +335,10 @@ class WorldManager():
 				if self.graph_scn.SceneInterface.nodeExists(nname):
 					self.graph_scn.SceneInterface.removeNode(nname)
 
-			deleteNodeInGraphicalAgent(world.scene.graphical_scene.root_node)
+			deleteNodeInGraphicalAgent(old_world.scene.graphical_scene.root_node)
 
 			verbose_print("REMOVE MARKERS...")
-			markers = world.scene.graphical_scene.markers
+			markers = old_world.scene.graphical_scene.markers
 
 			if not len(markers) == 0:
 				for marker in markers:
@@ -252,12 +347,16 @@ class WorldManager():
 						self.graph_scn.MarkersInterface.removeMarker(str(marker.name))
 
 
-	def removeWorldFromPhysicAgent(self, world):
+	def removeWorldFromPhysic(self, old_world):
+		""" Remove a World from all the physical components.
+		
+		:param old_world: a scene_pb2.World which will be deleted
+		"""
 		phy = self.phy
 
 		verbose_print("REMOVE PHYSICAL WORLD...")
 
-		for mechanism in world.scene.physical_scene.mechanisms:
+		for mechanism in old_world.scene.physical_scene.mechanisms:
 			mname = str(mechanism.name)
 			self.phy.s.deleteComponent(mname)
 
@@ -275,7 +374,7 @@ class WorldManager():
 				if to_del in self.phy.s.getComponents():
 					self.phy.s.deleteComponent(to_del)
 
-		for node in world.scene.physical_scene.nodes:
+		for node in old_world.scene.physical_scene.nodes:
 			removeRigidBodyChildren(node)
 
 		verbose_print("REMOVE UNUSED MATERIAL...")
@@ -283,20 +382,20 @@ class WorldManager():
 
 
 	def removeWorld(self, old_world):
+		""" Remove a World from all the physical & graphical components.
+		
+		:param old_world: a scene_pb2.World which will be deleted
 		"""
-		Remove everything included in old_world from the simulation.
-		"""
-		phy_name = self.phy.getName()
-
 		#delete graphical scene
-		self.removeWorldFromGraphicAgent(old_world)
+		self.removeWorldFromGraphic(old_world)
 
 		#delete physical scene
-		self.removeWorldFromPhysicAgent(old_world)
-
+		self.removeWorldFromPhysic(old_world)
 
 
 	def cleanGraph(self):
+		""" Clean all components in the graphical scene.
+		"""
 		if(self.graph is not None):
 			self.graph_scn.SceneInterface.clearScene()
 			self.graph_scn.MarkersInterface.clearMarkers()
@@ -305,6 +404,8 @@ class WorldManager():
 
 
 	def cleanPhy(self):
+		""" Clean all components in the physical scene.
+		"""
 		if(self.phy is not None):
 
 			self.ms.clean()
@@ -317,35 +418,61 @@ class WorldManager():
 
 
 	def stopSimulation(self):
+		""" Stop simulation, whithout stopping physic agent.
+		"""
 		self.phy.s.stopSimulation()
 
 	def startSimulation(self):
+		""" Start simulation when agent is already started, but the simulation has been stop with stopSimulation method.
+		"""
 		self.phy.s.startSimulation()
 
 
 	def addInteraction(self, pairs_of_contact):
+		""" Add the visualization of interactions between pairs of bodies.
+		
+		:param pairs_of_contact: a list of bodies name pairs.
+		
+		For instance, if we want to see the interactions between "b1"/"b2" &
+		between "b1"/"b3", the argument will be on the form:
+		addInteraction( [("b1", "b2"), ("b1", "b3")] )
+		"""
 		occ = self.phy.s.Connectors.OConnectorContactBody("occ")
 		for b1, b2 in pairs_of_contact:
 			occ.addInteraction(b1, b2)
 
 
 	def removeInteraction(self, pairs_of_contact):
+		""" Remove the visualization of interactions between pairs of bodies.
+		
+		:param pairs_of_contact: a list of bodies name pairs.
+		
+		For instance, if we want to unsee the interactions between "b1"/"b2" &
+		between "b1"/"b3", the argument will be on the form:
+		removeInteraction( [("b1", "b2"), ("b1", "b3")] )
+		"""
 		occ = self.phy.s.Connectors.OConnectorContactBody("occ")
 		for b1, b2 in pairs_of_contact:
 			occ.removeInteraction(b1, b2)
 
 
 	def removeAllInteractions(self):
+		""" Remove the visualization of all interactions already registered.
+		"""
 		occ = self.phy.s.Connectors.OConnectorContactBody("occ")
 		occ.removeAllInteractions()
 
 
 
 	def addMarkers(self, world, bodies_to_display=None, thin_markers=True):
-		"""
-		Add a visual frame to each body of bodies_to_display list.
-		If the list is empty, a visual frame is added for all body in world.
-		This must be call before addWorld
+		""" Add a visual frame to each body of bodies_to_display list.
+		
+		:param world: a scene_pb2.World where are added the new markers
+		:param bodies_to_display: a list of body name one wants to display
+		:param thin_markers: if True, displayed markers are three lines, else they are three big arrows
+		
+		If the list is empty, a visual frame is added for every body in world.
+		This must be call before the addWorld method.
 		"""
 		if (self.graph is None):
 			verbose_print("No graphic agent. Nothing to do.")
@@ -366,22 +493,42 @@ class WorldManager():
 			else:
 				verbose_print("Warning: "+body_name+" marker already exists. Nothing to do.")
 
+
 	def createMarkerWorld(self, world_name, marker_name_list):
+		""" Create a World which contains only markers.
+		
+		:param world_name: the name given to the new created world
+		:param marker_name_list: a list of names representing the new created markers
+		:rtype: a scene_pb2.World which contains the new markers
+		"""
 		marker_world = desc.scene.createWorld(name = world_name)
 		self.addFreeMarkers(marker_world, marker_name_list)
 
 		return marker_world
 
+
 	def addMarkerToSimulation(self, name, thin_markers=True):
+		""" Directly add a marker into the graphical scene.
+		
+		:param name: the name of the new marker
+		:param thin_markers: if True, displayed markers are three lines, else they are three big arrows
+		"""
 		self.graph_scn.MarkersInterface.addMarker(str(name), thin_markers)
 
 	def removeMarkerFromSimulation(self, name):
+		""" Directly remove a marker from the graphical scene.
+		
+		:param name: the name of the marker one wants to delete
+		"""
 		self.graph_scn.MarkersInterface.removeMarker(str(name))
 
 	def addFreeMarkers(self, world, marker_name_list):
-		"""
-		Add a free marker, in order to update its position,
-		write [(Hxdes, marker_name)] in a output port (name, "vector_pair_Displacementd_string")
+		""" Add free markers into a world, whose positions are not related to bodies.
+		
+		:param world: a scene_pb2.World where the marker will be added
+		:param marker_name_list: a string or a list corresponding to the name(s) of the new marker(s)
+		
+		In order to update its position, write [(Hxdes, marker_name)] in a output port (name, "vector_pair_Displacementd_string")
 		connected to graph.getPort("framePosition")
 		"""
 		if not hasattr(marker_name_list, '__iter__'):
@@ -393,9 +540,12 @@ class WorldManager():
 
 
 	def removeMarkers(self, world, bodies_to_hide=None):
-		"""
-		Remove the visual frame attached to each body of bodies_to_display list.
-		If the list is empty, a visual frame is removed for all body in world.
+		""" Remove the visual frame attached to each body of bodies_to_display list.
+		
+		:param world: a scene_pb2.World where the marker will be removed
+		:param bodies_to_display: a list of body name one wants to hide
+		
+		If the list is empty, visual frame for every body in world is removed.
 		"""
 		if (self.graph is None):
 			verbose_print("No graphic agent. Nothing to do.")
@@ -419,8 +569,8 @@ class WorldManager():
 
 
 	def createOConnectorContactBody(self, connector_name, port_name, body1_name, body2_name):
-		"""
-		Create the OConnectorContactBody and define the interaction for the pair (body1, body2).
+		""" Create the OConnectorContactBody and define the interaction for the pair (body1, body2).
+		
 		Return the ContactInfo data structure associated.
 		"""
 		phy = self.phy
